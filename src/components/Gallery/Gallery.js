e889d6efda5491a391a834e6c029b8cbb73bccd0
PecheritsaAleksey <fromzeya@gmail.com>
2021-01-05 11:58:48 +0700

    Temp change of way of getting images url

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,68 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
-import {
-    LazyLoadImage,
-    trackWindowScroll,
-} from "react-lazy-load-image-component";
+import { LazyLoadImage, trackWindowScroll } from "react-lazy-load-image-component";
 import ScrollUpButton from "react-scroll-up-button";
 import firebaseApp from "../../firebaseApp";
 
 import "./Gallery.css";
 
 const Gallery = (props) => {
-    const [storageRef, setStorageRef] = useState([]);
-    const [images, setImages] = useState([]);
+  const [storageRef, setStorageRef] = useState([]);
+  const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        if (props.location.state) {
-            const storageRef = firebaseApp.storage().ref(
-                `${props.location.state.folderPath}`
-            );
+  useEffect(() => {
+    if (props.location.state) {
+      const storageRef = firebaseApp.storage().ref(`${props.location.state.folderPath}`);
 
-            storageRef.listAll().then((result) => {
-                setStorageRef(result.items);
-            });
-        }
-    }, [props.location.state]);
+      storageRef.listAll().then((result) => {
+        setStorageRef(result.items);
+      });
+    }
+  }, [props.location.state]);
 
-    useEffect(() => {
-        if (props.location.state) {
-            let promiseArr = storageRef.map(function (imageRef) {
-                return imageRef.getDownloadURL().then(function (res) {
-                    return res;
-                });
-            });
+  useEffect(() => {
+    if (props.location.state) {
+      let imageUrlList = storageRef.map((imageRef) => {
+        return `https://firebasestorage.googleapis.com/v0/b/${
+          imageRef.location.bucket
+        }/o/${encodeURIComponent(imageRef.location.path_)}?alt=media`;
+      });
 
-            Promise.all(promiseArr).then(function (res) {
-                const { scrollPosition } = props;
-                const images = res.map((path) => (
-                    <LazyLoadImage
-                        className="image"
-                        effect="blur"
-                        key={path}
-                        scrollPosition={scrollPosition}
-                        src={path}
-                        width={1300}
-                        height={1300}
-                    />
-                ));
-                setImages(images);
-            });
-        } else {
-            setImages(<Redirect to="/" />);
-        }
-    }, [storageRef, props]);
+      const { scrollPosition } = props;
+      const images = imageUrlList.map((url) => (
+        <LazyLoadImage
+          className="image"
+          key={url}
+          scrollPosition={scrollPosition}
+          src={url}
+          width={1300}
+          height={1300}
+        />
+      ));
+      setImages(images);
+    } else {
+      setImages(<Redirect to="/" />);
+    }
+  }, [storageRef, props]);
 
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 
-    return (
-        <div>
-            {images}
-            <div>
-                <ScrollUpButton />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      {images}
+      <div>
+        <ScrollUpButton />
+      </div>
+    </div>
+  );
 };
 
 export default trackWindowScroll(Gallery);
